7b889de7644a6b49c7458e7d7931beec4391377c
CassBo <cassielbotello@gmail.com>
2025-10-15 16:36:11 -0600

    feat(layout): add breadcrumb componente and integrate into admin layout

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,3 +1,14 @@
-<x-admin-layout>
+<x-admin-layout :breadcrumbs="[
+    [
+        'name' => 'Dashboard',
+        'href' => route('admin.dashboard'),
+        'active' => request()->routeIs('admin.dashboard'),
+    ],
+    [
+        'name' => 'Profile',
+        'href' => route('profile.show'),
+        'active' => request()->routeIs('profile.show'),
+    ],
+]">
  Hola desde admin
 </x-admin-layout>
